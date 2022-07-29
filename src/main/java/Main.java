78543975ff64116c94fe1b13d1f4a9cc00813d69
Carlos <cortizmardones@gmail.com>
2022-07-28 22:43:34 -0400

    Se mejora logica de captura y tratamiento de excepciones

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,37 +2,51 @@ import javax.swing.JOptionPane;
 
 public class Main {
 
-	public static void main(String[] args) {
-		
-		//PRIMER EJERCICIO
-		int edad = Integer.parseInt(JOptionPane.showInputDialog("Introduce tu edad"));
-		String nombre = JOptionPane.showInputDialog("Introduce tu nombre");
-		Persona persona = new Persona(edad, nombre);
-		System.out.println(persona.toString());
-		
-		
-		//SEGUNDO EJERCICIO
-		String palabra = "escalera";
-		if(persona.verificarPalindromo(palabra)) {
-			System.out.println("La palabra: '" + palabra + "'" + " si es un palindromo");
-		} else {
-			System.out.println("La palabra: '" + palabra + "'" + " no es un palindromo");
+	public static void main(String[] args) throws Exception {
+	
+		try {
+			
+			//PRIMER EJERCICIO
+			int edad = Integer.parseInt(JOptionPane.showInputDialog("Introduce tu edad"));
+			String nombre = JOptionPane.showInputDialog("Introduce tu nombre");
+			Persona persona = new Persona(edad, nombre);
+			System.out.println(persona.toString());
+			
+			
+			//SEGUNDO EJERCICIO
+			String palabra = JOptionPane.showInputDialog("Introduce una palabra , para verificar si es un palindromo");
+			if(persona.verificarPalindromo(palabra)) {
+				System.out.println("La palabra: '" + palabra + "'" + " si es un palindromo");
+			} else {
+				System.out.println("La palabra: '" + palabra + "'" + " no es un palindromo");
+			}
+			
+			//TERCER EJERCICIO
+			int[] arrayDesordenado = new int[6];
+			arrayDesordenado[0] = 40;
+			arrayDesordenado[1] = 9;
+			arrayDesordenado[2] = 55;
+			arrayDesordenado[3] = 1;
+			arrayDesordenado[4] = 76;
+			arrayDesordenado[5] = 101;
+
+			int[] arrayOrdenado = persona.retornarArrayOrdenado(arrayDesordenado);
+
+			for (int i = 0; i < arrayOrdenado.length; i++) {
+				System.out.println(arrayOrdenado[i]);
+			}
+			
+		} catch (Exception e) {
+			
+			// IMPRIMO LA TRAZA DEL ERROR
+			e.printStackTrace();
+			
+			// LANZO UN EXCEPCION PARA DETENER LA EJECUCION
+			throw new Exception(e);
+			
 		}
 		
-		//TERCER EJERCICIO
-		int[] arrayDesordenado = new int[6];
-		arrayDesordenado[0] = 40;
-		arrayDesordenado[1] = 9;
-		arrayDesordenado[2] = 55;
-		arrayDesordenado[3] = 1;
-		arrayDesordenado[4] = 76;
-		arrayDesordenado[5] = 101;
 
-		int[] arrayOrdenado = persona.retornarArrayOrdenado(arrayDesordenado);
-
-		for (int i = 0; i < arrayOrdenado.length; i++) {
-			System.out.println(arrayOrdenado[i]);
-		}
 
 	}
 
